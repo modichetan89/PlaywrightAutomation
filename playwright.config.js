0bf3eecb2e6dc2d21c0d30c920c03fb6a6ea5ff4
Chetan <modichetan89@gmail.com>
2025-01-04 00:10:04 +0530

    set headless as true

--- playwright.config.js
+++ playwright.config.js
@@ -35,7 +35,7 @@ module.exports = defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     browserName: 'chromium',
-    headless: false,
+    headless: true,
     trace: 'on-first-retry',
   },
 
